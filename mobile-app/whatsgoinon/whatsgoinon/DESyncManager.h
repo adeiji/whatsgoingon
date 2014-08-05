49d02eb037383826e64cdb2f22688d858a368b11
adeiji <adeiji@yahoo.com>
2014-08-05 15:25:55 -0700

    Finished adding all the screens for the UI and all the dependencies necessary for Parse

---
+++ mobile-app/whatsgoinon/whatsgoinon/DESyncManager.h
@@ -0,0 +1,13 @@
+//
+//  DESyncManager.h
+//  whatsgoinon
+//
+//  Created by adeiji on 8/5/14.
+//  Copyright (c) 2014 adeiji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DESyncManager : NSObject
+
+@end
---
+++ mobile-app/whatsgoinon/whatsgoinon/DESyncManager.m
@@ -0,0 +1,13 @@
+//
+//  DESyncManager.m
+//  whatsgoinon
+//
+//  Created by adeiji on 8/5/14.
+//  Copyright (c) 2014 adeiji. All rights reserved.
+//
+
+#import "DESyncManager.h"
+
+@implementation DESyncManager
+
+@end
